362098c2707e9f2d475dccf1aa7237a0718b347d
kobe73er <kobe73er@gmail.com>
2023-06-09 00:00:31 +0800

    add one more point for testing pipeline

--- src/ProductManageController.ts
+++ src/ProductManageController.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Param, Query} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller("")
+@Controller("product")
 export class ProductManageController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/product/all")
+  @Get("/all")
   getAllProducts(): string {
     return this.appService.getAllProducts();
   }
+
+  @Get("/all2")
+  getAllProducts2(): string {
+    return "all2";
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import {Get, Injectable, Param} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
